b9af3b13bfa0da16c34522647a3a7529380d22e9
Ilya.w <87799207+ilyaw39@users.noreply.github.com>
2022-03-15 17:22:52 +0800

    Updated files

--- packages/org-bullets-20200317.1740/org-bullets-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from org-bullets.el  -*- no-byte-compile: t -*-
-(define-package "org-bullets" "20200317.1740" "Show bullets in org-mode as UTF-8 characters" 'nil :commit "767f55feb58b840a5a04eabfc3fbbf0d257c4792" :authors '(("sabof")) :maintainer '("D. Williams" . "d.williams@posteo.net") :url "https://github.com/integral-dw/org-bullets")
