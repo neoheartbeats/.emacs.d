6b40218c1546eff27fcdd01412556ea5a2a09be0
Ilya.w <87799207+ilyaw39@users.noreply.github.com>
2022-03-22 14:37:15 +0800

    Updated files

--- modules/ui.el
+++ modules/ui.el
@@ -1,3 +1,4 @@
+
 ;; ui.el --- Lyrith: loading first -*- lexical-binding: t -*-
 ;;
 ;; Copyright © 2022 Ilya.w
@@ -106,6 +107,36 @@
 ;;
 ;; Hide mode line
 (setq-default mode-line-format nil)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Animations
+;;
+;; Startup the feature
+(mac-start-animation nil
+                     :type 'page-curl-with-shadow
+                     :duration 1.0
+                     :direction 'right
+                     :angle 45)
+
+;; Implement fade-outs
+(defcustom mac-animation-duration 0.3
+  "Duration of transition animations")
+(defvar mac-animation-locked-p nil)
+(defun mac-animation-toggle-lock ()
+  (setq mac-animation-locked-p (not mac-animation-locked-p)))
+
+(defun animate-frame-fade-out (&rest args)
+  (unless mac-animation-locked-p
+    (mac-animation-toggle-lock)
+    (mac-start-animation nil :type 'fade-out :duration mac-animation-duration)
+    (run-with-timer mac-animation-duration nil 'mac-animation-toggle-lock)))
+
+(advice-add 'set-window-buffer :before 'animate-frame-fade-out)
+(advice-add 'split-window :before 'animate-frame-fade-out)
+(advice-add 'delete-window :before 'animate-frame-fade-out)
+(advice-add 'delete-other-windows :before 'animate-frame-fade-out)
+(advice-add 'window-toggle-side-windows :before 'animate-frame-fade-out)
 
 (provide 'ui)
 
