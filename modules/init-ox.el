a9fd0a4629c8b4186ea3ee37c3bcf673c65b0eaa
Ilya.w <87799207+ilyaw39@users.noreply.github.com>
2022-09-12 23:41:05 +0800

    Created init-ox.el

---
+++ modules/init-ox.el
@@ -0,0 +1,18 @@
+;; init-ox.el --- Credits: loading first -*- lexical-binding: t -*-
+;;
+;; Copyright © 2022 Ilya.w
+;;
+;; Author: Ilya.w <ilya.w@icloud.com>
+;;
+;; This file is not part of GNU Emacs.
+;;
+;; Commentary:
+;;
+;; This file provides the basis & structure that draws Emacs configuration.
+;;
+;; Code:
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Setup `ox-hugo'
+(use-package ox-hugo)
