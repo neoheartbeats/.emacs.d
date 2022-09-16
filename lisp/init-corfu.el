3d930f98fb7d89ebc4a6c366c6d7f3192d9d269a
Ilya.w <87799207+ilyaw39@users.noreply.github.com>
2022-09-16 09:57:21 +0800

    Added extensions

--- lisp/init-corfu.el
+++ lisp/init-corfu.el
@@ -35,5 +35,16 @@
 	((prog-mode org-mode) . corfu-mode))
 
 
+;; Add extensions
+(use-package cape
+  :init
+  ;; Add `completion-at-point-functions', used by `completion-at-point'.
+  (add-to-list 'completion-at-point-functions #'cape-dabbrev)
+  (add-to-list 'completion-at-point-functions #'cape-file)
+  (add-to-list 'completion-at-point-functions #'cape-history)
+  (add-to-list 'completion-at-point-functions #'cape-abbrev)
+  (add-to-list 'completion-at-point-functions #'cape-ispell))
+
+
 (provide 'init-corfu)
 ;;; init-corfu.el ends here
--- lisp/init-tex.el
+++ lisp/init-tex.el
@@ -22,7 +22,7 @@
       '( ;; Ensure LaTeX fragments can be displayed correctly on dark backgrounds
 		    :foreground default
         :background "Transparent"
-        :scale 1.0
+        :scale 3.5
         :html-foreground "Black"
         :html-background "Transparent"
         :html-scale 1.2
