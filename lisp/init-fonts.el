5c4873cde25f1e3765d4708a8b91ad68c17f592d
Ilya.w <87799207+ilyaw39@users.noreply.github.com>
2022-09-20 21:06:02 +0800

    Modified UI

--- lisp/init-fonts.el
+++ lisp/init-fonts.el
@@ -11,8 +11,8 @@
 
 (set-fontset-font "fontset-default" 'unicode "SF Pro")
 (set-fontset-font "fontset-default" 'emoji "Apple Color Emoji")
-(set-fontset-font "fontset-default" 'han "HarmonyOS Sans SC")
-(set-fontset-font "fontset-default" 'kana "HarmonyOS Sans SC")
+(set-fontset-font "fontset-default" 'han "Noto Serif CJK SC")
+(set-fontset-font "fontset-default" 'kana "Noto Serif CJK SC")
 
 
 ;; No gc for font caches
