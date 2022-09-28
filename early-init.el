675f9194717f71e23e85036c22c08a6e5a5bd1d5
Ilya.w <87799207+ilyaw39@users.noreply.github.com>
2022-09-28 19:56:12 +0800

    Update early-init.el

--- early-init.el
+++ early-init.el
@@ -15,7 +15,7 @@
 (dolist (var '(default-frame-alist initial-frame-alist))
   (add-to-list var '(width . (text-pixels . 1920)))
   (add-to-list var '(height . (text-pixels . 990)))
-  (add-to-list vat '(alpha . (95 . 95))))
+  (add-to-list var '(alpha . (90 . 90))))
 
 ;; Faster to disable these here (before they've been initialized)
 (push '(menu-bar-lines . 0) default-frame-alist)
