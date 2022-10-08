6cf4a91bab11cdfe0934ccac826f21a3d3146daf
Ilya.w <87799207+ilyaw39@users.noreply.github.com>
2022-10-08 15:37:57 +0800

    Fixed bugs

--- lisp/init-tex.el
+++ lisp/init-tex.el
@@ -14,26 +14,26 @@
 
 
 ;; Setup `dvisvgm' to preview LaTeX fragments
-(setq org-preview-latex-default-process 'dvisvgm)
-(setq org-preview-latex-process-alist
-  '((dvisvgm
-			:programs ("xelatex" "dvisvgm")
-			:description "xdv > svg"
-			:image-input-type "xdv"
-			:image-output-type "svg"
-			:image-size-adjust (1.7 . 1.5)
-      :post-clean nil
-      :latex-header nil
-			:latex-compiler ; Default `xelatex' as the process previewing LaTeX fragments
-			("xelatex -no-pdf -interaction nonstopmode -output-directory %o %f")
-			:image-converter ; Set `dvisvgm' with --exact option
-			("dvisvgm %f -n -e -b 1 -c %S -o %O"))))
+(setq org-preview-latex-default-process 'imagemagick)
+;; (setq org-preview-latex-process-alist
+;;   '((dvisvgm
+;; 			:programs ("xelatex" "dvisvgm")
+;; 			:description "xdv > svg"
+;; 			:image-input-type "xdv"
+;; 			:image-output-type "svg"
+;; 			:image-size-adjust (1.7 . 1.5)
+;;       :post-clean nil
+;;       :latex-header nil
+;; 			:latex-compiler ; Default `xelatex' as the process previewing LaTeX fragments
+;; 			("xelatex -no-pdf -interaction nonstopmode -output-directory %o %f")
+;; 			:image-converter ; Set `dvisvgm' with --exact option
+;; 			("dvisvgm %f -n -e -b 1 -c %S -o %O"))))
 
 (setq org-format-latex-options
   '( ; Ensure LaTeX fragments can be displayed correctly on dark backgrounds
 		 :foreground default
      :background "Transparent"
-     :scale 3.5
+     :scale 1.0
      :html-foreground "Black"
      :html-background "Transparent"
      :html-scale 1.2
