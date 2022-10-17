87a318f67b26de82704612b56e0c0d6dedecc016
Ilya.w <87799207+ilyaw39@users.noreply.github.com>
2022-10-18 07:30:44 +0800

    Enhanced LaTeX rendering

--- lisp/init-tex.el
+++ lisp/init-tex.el
@@ -20,7 +20,7 @@
 (setq-default org-latex-preview-ltxpng-directory "~/.emacs.d/ltximg/")
 
 
-;; Setup `dvisvgm' to preview LaTeX fragments
+;; Setup `imagemagick' to preview LaTeX fragments
 (setq org-preview-latex-default-process 'imagemagick)
 (setq org-preview-latex-process-alist
       '((imagemagick
@@ -31,16 +31,6 @@
          :image-size-adjust (1.0 . 1.0)
          :latex-compiler ("xelatex -interaction nonstopmode -output-directory %o %f")
          :image-converter ("convert -density %D -trim -antialias %f -quality 100 %O"))))
-
-;; (setq org-preview-latex-process-alist
-;;       '((dvisvgm
-;;          :programs ("xelatex" "dvisvgm")
-;;          :description "xdv > svg"
-;;          :image-input-type "xdv"
-;;          :image-output-type "svg"
-;;          :image-size-adjust (1.7 . 1.5)
-;;          :latex-compiler ("xelatex -no-pdf -interaction nonstopmode -output-directory %o %f")
-;;          :image-converter ("dvisvgm %f -e -n -b 1 -c %S -o %O"))))
 
 (setq org-format-latex-options
       '( ; Ensure LaTeX fragments can be displayed correctly on dark backgrounds
@@ -58,7 +48,7 @@
       '(("" "mathtools" t)
         ("" "physics" t)
         ("" "mhchem" t)
-	("" "siunitx" t)))
+        ("" "siunitx" t)))
 
 
 ;;; Better LaTeX editor for Org mode
@@ -69,13 +59,55 @@
    (org-mode . turn-on-org-cdlatex)))
 
 ;; Syntax highlighting for LaTeX in Org mode
-(setq org-highlight-latex-and-related '(latex))
+(setq org-highlight-latex-and-related '(script))
 
 
 (use-package ox-latex
   :straight (:type built-in))
 
 (setq-default org-latex-compiler "xelatex")
+
+
+(defun my/org-latex--get-tex-string ()
+  "Return the content of the LaTeX fragment at point."
+  (let ((datum (org-element-context)))
+    (org-element-property :value datum)))
+
+(defun my/latex-fragment-superscript-p ()
+  "Return `t' if '^' in current LaTeX fragment."
+  (memq 94 (string-to-list (my/org-latex--get-tex-string))))
+
+(defun my/latex-fragment-subscript-p ()
+  "Return `t' if '_' in current LaTeX fragment."
+  (memq 95 (string-to-list (my/org-latex--get-tex-string))))
+
+(defun my/latex-fragment-script-p ()
+  "Return `t' if both '_' &  '^' in current LaTeX fragment."
+  (and (memq 94 (string-to-list (my/org-latex--get-tex-string)))
+       (memq 95 (string-to-list (my/org-latex--get-tex-string)))))
+
+(defun org--make-preview-overlay (beg end image &optional imagetype)
+  "Build an overlay between BEG and END using IMAGE file.
+Argument IMAGETYPE is the extension of the displayed image,
+as a string.  It defaults to \"png\"."
+  (setq my/position 'center)
+  (cond ((my/latex-fragment-script-p)
+         (setq my/position 'center))
+        ((my/latex-fragment-superscript-p)
+         (setq my/position 100))
+        ((my/latex-fragment-subscript-p)
+         (setq my/position 70)))
+  (let ((ov (make-overlay beg end))
+	(imagetype (or (intern imagetype) 'png)))
+    (overlay-put ov 'org-overlay-type 'org-latex-overlay)
+    (overlay-put ov 'evaporate t)
+    (overlay-put ov
+		 'modification-hooks
+		 (list (lambda (o _flag _beg _end &optional _l)
+			 (delete-overlay o))))
+    (overlay-put ov
+		 'display
+		 (list 'image :type imagetype :file image :ascent my/position))))
 
 
 (provide 'init-tex)
