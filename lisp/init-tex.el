16b680404bf25decee5c273bf6627d7ee4f5cee7
Ilya.w <87799207+ilyaw39@users.noreply.github.com>
2022-10-28 07:18:47 +0800

    Adjusted LaTeX preview

--- lisp/init-tex.el
+++ lisp/init-tex.el
@@ -90,7 +90,7 @@ as a string.  It defaults to \"png\"."
       '( ;; Ensure LaTeX fragments can be displayed correctly on dark backgrounds
 	:foreground default
         :background "Transparent"
-        :scale 3.5
+        :scale 3.3
         :html-foreground default
         :html-background "Transparent"
         :html-scale 1.2
