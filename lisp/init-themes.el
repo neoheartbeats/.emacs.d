1d1ac54eb925dc6004691911dc0ce63e4b7e1f94
Ilya.w <87799207+ilyaw39@users.noreply.github.com>
2022-11-22 21:00:31 +0800

    Modified UI

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -7,17 +7,17 @@
 
 
 ;;; Modus Themes
-(use-package emacs
-  :init
-  (setq modus-themes-bold-constructs t)
-  (setq modus-themes-syntax '(green-strings))
-  (setq modus-themes-links '(no-color))
-  (setq modus-themes-prompts '(intense bold))
-  (setq modus-themes-mode-line '(borderless))
-  (setq modus-themes-region '(bg-only no-extend))
-  (setq modus-themes-headings '((t . (rainbow))))
-  :config
-  (load-theme 'modus-vivendi t))
+;; (use-package emacs
+;;   :init
+;;   (setq modus-themes-bold-constructs t)
+;;   (setq modus-themes-syntax '(green-strings))
+;;   (setq modus-themes-links '(no-color))
+;;   (setq modus-themes-prompts '(intense bold))
+;;   (setq modus-themes-mode-line '(borderless))
+;;   (setq modus-themes-region '(bg-only no-extend))
+;;   (setq modus-themes-headings '((t . (rainbow))))
+;;   :config
+;;   (load-theme 'modus-vivendi t))
 
 
 ;;; Mode line settings
@@ -25,10 +25,10 @@
 
 
 ;;; Ef Themes
-;; (use-package ef-themes
-;;   :config
-;;   (mapc #'disable-theme custom-enabled-themes)
-;;   (load-theme 'ef-bio :no-confirm))
+(use-package ef-themes
+  :config
+  (mapc #'disable-theme custom-enabled-themes)
+  (load-theme 'ef-dark :no-confirm))
 
 
 (provide 'init-themes)
