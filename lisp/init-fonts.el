68175014dc4a29f16acb3e3115aa45f7b3bb23a3
Ilya.w <87799207+ilyaw39@users.noreply.github.com>
2022-12-01 17:44:13 +0800

    Modified fonts

--- lisp/init-fonts.el
+++ lisp/init-fonts.el
@@ -7,11 +7,11 @@
 	            :height 140)
 
 (set-face-attribute 'variable-pitch nil
-                    :font "HarmonyOS Sans SC")
+                    :font "Noto Serif CJK SC")
 
 (set-fontset-font "fontset-default" 'unicode "PragmataPro")
-(set-fontset-font "fontset-default" 'han "HarmonyOS Sans SC")
-(set-fontset-font "fontset-default" 'kana "HarmonyOS Sans SC")
+(set-fontset-font "fontset-default" 'han "Noto Serif CJK SC")
+(set-fontset-font "fontset-default" 'kana "Noto Serif CJK SC")
 
 
 ;; No gc for font caches
