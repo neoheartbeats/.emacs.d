25d3e01ff968eb83186d2edbd15ed716b0cee254
Ilya.w <87799207+ilyaw39@users.noreply.github.com>
2023-01-01 03:54:27 +0800

    Updated files

--- lisp/init-dirs.el
+++
@@ -1,21 +0,0 @@
-;;; init-dirs.el --- File management in Emacs -*- lexical-binding: t -*-
-;;; Commentary:
-
-;; This file provides functions for file management based on 'dirvish`
-;; and additionally provides file applications support.
-
-;;; Code:
-
-
-;; File management with `dirvish'
-(use-package dirvish
-  :defer t
-  :init
-  (dirvish-override-dired-mode 1)
-  (global-unset-key (kbd "s-d"))
-  :bind
-  (("s-d d" . dirvish)))
-
-
-(provide 'init-dirs)
-;;; init-dirs.el ends here
--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -8,6 +8,16 @@
 
 (use-package modus-themes
   :init
+  (setq-default modus-vivendi-palette-overrides
+                `(
+                  (fringe unspecified)
+                  ;; From the section "Make the mode line borderless"
+                  (border-mode-line-active unspecified)
+                  (border-mode-line-inactive unspecified)
+
+                  ;; From the section "Make matching parenthesis more or less intense"
+                  (bg-paren-match bg-magenta-intense)
+                  (underline-paren-match fg-main)))
   (setq modus-themes-region '(no-extend))
   :config
   (mapc #'disable-theme custom-enabled-themes)
