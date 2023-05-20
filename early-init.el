ca5c2bb54c377bb06da5ef0467a691b08a559693
Ilya.w <87799207+ilyaw39@users.noreply.github.com>
2023-05-20 16:03:54 +0800

    Add Python support

--- early-init.el
+++ early-init.el
@@ -20,8 +20,9 @@
 ;; Enhance darwing the frame when initialization
 ;;
 ;; Faster to disable these here (before they've been initialized)
-(push '(width . 145) default-frame-alist)
+(push '(width . 175) default-frame-alist)
 (push '(height . 58) default-frame-alist)
+(push '(alpha . (90 . 90)) default-frame-alist)
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
--- lisp/init-temp.el
+++ lisp/init-temp.el
@@ -1,30 +1,41 @@
 ;; init-comp.el ---  Modern template system -*- lexical-binding: t -*-
 ;;
-;; Copyright (C) 2022-2023 Ilya Wang
-;;
 ;; This file is not part of GNU Emacs.
 ;;
 ;; Commentary:
+;;
+;; Despite `Yasnippet' is not been frequently updated, it still got its high
+;; compatibilities.
+;;
 ;; Code:
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;; Configure Tempel
-(use-package tempel
-  :ensure t
-  :bind
-  (("s-." . tempel-expand)
-   ("s-/" . tempel-insert))
-  :init ; Setup completion at point
-  (defun tempel-setup-capf ()
-    "Add the Tempel Capf to `completion-at-point-functions'."
-    (setq-local completion-at-point-functions
-                (cons #'tempel-expand
-                      completion-at-point-functions)))
-
-  (add-hook 'org-mode-hook #'tempel-setup-capf)
+;; (use-package tempel
+;;   :ensure t
+;;   :bind
+;;   (("s-." . tempel-expand)
+;;    ("s-/" . tempel-insert))
+;;   :init ; Setup completion at point
+;;   (defun tempel-setup-capf ()
+;;     "Add the Tempel Capf to `completion-at-point-functions'."
+;;     (setq-local completion-at-point-functions
+;;                 (cons #'tempel-expand
+;;                       completion-at-point-functions)))
+;;
+;;   (add-hook 'org-mode-hook #'tempel-setup-capf)
+;;   :config
+;;   (setq tempel-path "~/.emacs.d/temp.eld"))
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Configure `Yasnippet'
+(use-package yasnippet :ensure t
   :config
-  (setq tempel-path "~/.emacs.d/temp.eld"))
+  (setq yas-visit-from-menu t)
+  (setq yas-triggers-in-field t)
+  (yas-global-mode 1))
 
 
 (provide 'init-temp)
