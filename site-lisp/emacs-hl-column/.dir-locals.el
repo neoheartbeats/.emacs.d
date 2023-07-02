25ba5656cd4c910ebd9243a79e0fb3919ad9d837
Ilya.w <87799207+ilyaw39@users.noreply.github.com>
2023-07-02 12:00:35 +0800

    Update Org to TEC's develop branch

--- site-lisp/emacs-hl-column/.dir-locals.el
+++
@@ -1,5 +0,0 @@
-;;; Directory Local Variables            -*- no-byte-compile: t -*-
-;;; For more information see (info "(emacs) Directory Variables")
-
-((nil . ((fill-column . 70)
-         (indent-tabs-mode . nil))))
