2371c2be68d203496f7eed9e9174c9f60e9c712f
Ilya.w <87799207+ilyaw39@users.noreply.github.com>
2024-03-04 18:14:30 +0800

    Add Java support for Org-mode

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -214,6 +214,7 @@
 (org-babel-do-load-languages 'org-babel-load-languages
                              '((emacs-lisp . t)
                                (python . t)
+			       (java . t)
                                (shell . t)))
 
 ;; Useful functions
