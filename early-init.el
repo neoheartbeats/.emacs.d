1e72ed8722980d8e27958d2a8184740f3ec6a349
sthenno <sthenno@xxsp.lan>
2026-05-27 22:32:00 -0700

    chore(misc)

--- early-init.el
+++ early-init.el
@@ -1,4 +1,4 @@
-;;; early-init.el --- Early initialization settings -*- lexical-binding: t; no-byte-compile: t -*-
+;;; early-init.el --- Early initialization settings -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2021-2026 Sthenno <sthenno@sthenno.com>
 
@@ -10,45 +10,37 @@
 
 ;;; Code:
 
-;;; Startup
-
-(setq-default gc-cons-threshold (* 512 1024 1024)
-              gc-cons-percentage 1.0)
-(add-hook 'emacs-startup-hook #'(lambda ()
-                                  (setq gc-cons-threshold (* 128 1024 1024)
-                                        gc-cons-percentage 0.1)))
-(require 'cl-lib)
-
-;;; Loading
-(setq load-prefer-newer t)
-(setq load-path-filter-function #'load-path-filter-cache-directory-files)
-
-(setq package-install-upgrade-built-in t
+;;; Loading and packages
+(setq load-prefer-newer t
+      load-path-filter-function #'load-path-filter-cache-directory-files
+      package-install-upgrade-built-in t
       package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
                          ("nongnu" . "https://elpa.nongnu.org/nongnu/")
                          ("melpa" . "https://melpa.org/packages/"))
-      package-selected-packages '(auctex consult corfu denote denote-journal denote-org
-                                         gptel magit marginalia ultra-scroll vertico yasnippet))
-
-;;; Encoding
-(set-charset-priority 'unicode)
-(prefer-coding-system 'utf-8)
+      package-selected-packages '(auctex
+                                  consult
+                                  corfu
+                                  denote
+                                  denote-journal
+                                  denote-org
+                                  gptel
+                                  magit
+                                  marginalia
+                                  ultra-scroll
+                                  vertico))
 
 ;;; Frames
-(setq frame-resize-pixelwise t)
-(setq-default default-frame-alist '((menu-bar-lines . 0) (tool-bar-lines . 0)
-                                    (vertical-scroll-bars) (horizontal-scroll-bars)
-                                    (height . 45) (width . 120)
-                                    (top . 0.5) (left . 0.5)
+(setq-default frame-resize-pixelwise t
+              default-frame-alist '((menu-bar-lines . 0)
+                                    (tool-bar-lines . 0)
+                                    (vertical-scroll-bars)
+                                    (horizontal-scroll-bars)
+                                    (height . 45)
+                                    (width . 120)
                                     (undecorated . t)
-                                    (alpha-background . 0.60) (ns-background-blur . 20)
-                                    (ns-alpha-elements . (ns-alpha-default
-                                                          ns-alpha-fringe
-                                                          ns-alpha-box
-                                                          ns-alpha-stipple
-                                                          ns-alpha-relief
-                                                          ns-alpha-glyphs))))
-
-(setq-default initial-frame-alist default-frame-alist)
+                                    (alpha . (0.8 . 0.8))
+                                    (top . 0.5)
+                                    (left . 0.5))
+              initial-frame-alist default-frame-alist)
 
 (provide 'early-init)
--- init.el
+++ init.el
@@ -6,28 +6,30 @@
 
 ;;; Commentary:
 
-;; Main Emacs configuration. Personal library code lives in user-lisp/.
+;; Main single-file Emacs configuration.
 
 ;;; Code:
 
 (setq custom-file (make-temp-file ".emacs-custom"))
 
+(declare-function consult-imenu-multi "consult-imenu" (&optional query))
+(declare-function corfu-history-mode "corfu-history" (&optional arg))
+(declare-function corfu-popupinfo-mode "corfu-popupinfo" (&optional arg))
+(declare-function gptel-make-openai "gptel-openai" (name &rest args))
+(declare-function vertico-directory-delete-char "vertico-directory" (n))
+(declare-function vertico-directory-enter "vertico-directory" (&optional arg))
+
 
-;;; Core startup and UI state
-(setopt save-silently t
-        remote-file-name-inhibit-locks t
-        backup-inhibited t
-        redisplay-skip-fontification-on-input t
-        fill-column 100
-        mode-line-format ""
-        header-line-format ""
-        user-full-name user-login-name
-        user-mail-address "sthenno@sthenno.com")
+;;; Startup
 (setopt inhibit-startup-screen t
         inhibit-startup-echo-area-message user-login-name
         inhibit-startup-buffer-menu t
         inhibit-default-init t
         initial-scratch-message "")
+
+(setopt user-full-name user-login-name
+        user-mail-address "sthenno@sthenno.com")
+
 (setopt menu-bar-mode nil
         scroll-bar-mode nil
         tool-bar-mode nil
@@ -40,41 +42,64 @@
   (message "Funding for this program was made possible by viewers like you."))
 
 
-;;; System essentials
+;;; Platform
 (setopt mac-option-modifier 'meta
         mac-command-modifier 'super)
-(setopt switch-to-prev-buffer-skip 0
-        save-place-autosave-interval 300
-        elisp-fontify-semantically t
-        ring-bell-function #'ignore
-        use-short-answers t
-        use-dialog-box nil
-        indent-tabs-mode nil
-        insert-directory-program (or (executable-find "gls")
-                                     insert-directory-program)
-        backward-delete-char-untabify-method 'hungry
-        kill-do-not-save-duplicates t
-        copy-region-blink-predicate #'ignore
-        sentence-end-double-space nil
-        save-interprogram-paste-before-kill t
+
+
+;;; Files
+(setopt save-silently t
+        remote-file-name-inhibit-locks t
+        backup-inhibited t
         global-auto-revert-mode t)
 
+
+;;; Windows
+(setopt redisplay-skip-fontification-on-input t
+        mode-line-compact t
+        header-line-format ""
+        switch-to-prev-buffer-skip 0)
+
+
+;;; Editing
+(setopt fill-column 100
+        elisp-fontify-semantically t
+        indent-tabs-mode nil
+        backward-delete-char-untabify-method 'hungry
+        kill-do-not-save-duplicates t
+        sentence-end-double-space nil
+        save-interprogram-paste-before-kill t)
+
+
+;;; Interaction
+(setopt ring-bell-function #'ignore
+        use-short-answers t
+        use-dialog-box nil
+        copy-region-blink-predicate #'ignore)
+
+
+;;; Persistence
 (require 'savehist)
-(setopt save-place-mode t
+(add-to-list 'savehist-additional-variables 'corfu-history)
+
+(setopt save-place-autosave-interval 300
+        save-place-mode t
         savehist-mode t
         electric-pair-mode t
-        electric-indent-mode t
         delete-selection-mode t)
 
-(keymap-global-set "s-q" #'kill-emacs)
-(keymap-global-set "s-w" #'kill-current-buffer)
-(keymap-global-set "s-e" #'delete-window)
-(keymap-global-set "s-d" #'find-file)
-(keymap-global-set "s-<right>" #'switch-to-next-buffer)
-(keymap-global-set "s-<left>" #'switch-to-prev-buffer)
-(keymap-global-set "<escape>" #'keyboard-escape-quit)
-(keymap-global-set "M-<down>" #'forward-paragraph)
-(keymap-global-set "M-<up>" #'backward-paragraph)
+
+;;; Key bindings
+(dolist (binding '(("s-q" . kill-emacs)
+                   ("s-w" . kill-current-buffer)
+                   ("s-e" . delete-window)
+                   ("s-d" . find-file)
+                   ("s-<right>" . switch-to-next-buffer)
+                   ("s-<left>" . switch-to-prev-buffer)
+                   ("<escape>" . keyboard-escape-quit)
+                   ("M-<down>" . forward-paragraph)
+                   ("M-<up>" . backward-paragraph)))
+  (keymap-global-set (car binding) (cdr binding)))
 
 (defun sthenno/delete-to-beginning-of-line ()
   "Delete text from point to the beginning of the current line."
@@ -89,15 +114,19 @@
     (save-restriction
       (widen)
       (let ((inhibit-message t))
-        (lisp-indent-region (point-min) (point-max))))))
-(keymap-set emacs-lisp-mode-map "s-i" #'sthenno/lisp-indent-buffer)
-(keymap-set emacs-lisp-mode-map "C-c C-c" #'emacs-lisp-byte-compile-and-load)
-(keymap-set emacs-lisp-mode-map "s-k" #'kill-sexp)
-(keymap-set emacs-lisp-mode-map "M-<backspace>" #'backward-kill-sexp)
+        (lisp-indent-region (point-min) (point-max))
+        (delete-trailing-whitespace)))))
+(dolist (binding '(("s-i" . sthenno/lisp-indent-buffer)
+                   ("C-c C-c" . emacs-lisp-byte-compile-and-load)
+                   ("s-k" . kill-sexp)
+                   ("M-<backspace>" . backward-kill-sexp)))
+  (keymap-set emacs-lisp-mode-map (car binding) (cdr binding)))
 
 ;;; Dired
 (with-eval-after-load 'dired
-  (setopt dired-no-confirm t
+  (setopt insert-directory-program (or (executable-find "gls")
+                                       insert-directory-program)
+          dired-no-confirm t
           dired-use-ls-dired t
           dired-hide-details-hide-information-lines t
           dired-hide-details-hide-absolute-location t
@@ -113,12 +142,10 @@
 
 ;;; Appearance
 (require-theme 'modus-themes)
-;; (setopt modus-themes-common-palette-overrides '((fg-line-number-active fg-dim)
-;;                                                 (bg-line-number-active bg-hl-line)
-;;                                                 (fg-line-number-inactive "#535353")
-;;                                                 (bg-line-number-inactive unspecified)
-;;                                                 (fg-paren-match unspecified)
-;;                                                 (bg-paren-match unspecified)))
+(setopt modus-themes-common-palette-overrides '((fg-paren-match unspecified)
+                                                (bg-paren-match unspecified)
+                                                (border bg-main)
+                                                (fringe bg-main)))
 (load-theme 'modus-vivendi :no-confirm)
 
 (set-face-attribute 'default nil :family "Tempestypes" :height 140)
@@ -126,69 +153,81 @@
 (set-face-attribute 'fill-column-indicator nil :height 0.1)
 (set-face-attribute 'show-paren-match nil
                     :background 'unspecified :foreground "green" :box '(:line-width (-1 . -1)))
-;; (set-face-attribute 'line-number nil :font (font-spec :family "Noto Serif CJK SC"))
-;; (set-face-attribute 'line-number-current-line nil :font (font-spec :family "Noto Serif CJK SC"))
 
-(dolist (face '(mode-line mode-line-active mode-line-inactive))
-  (set-face-attribute face nil
-                      :background 'unspecified :foreground "#535353" :box nil
-                      :underline t :height 0.1))
-
-(let ((font "Noto Serif CJK SC"))
+;;; Fonts
+(let ((font "PingFang SC"))
   (dolist (charset '(kana han cjk-misc))
     (set-fontset-font t charset (font-spec :family font))))
-;; (set-fontset-font t 'emoji (font-spec :family "Apple Color Emoji"))
-;; (set-fontset-font t 'ucs (font-spec :family "SF Pro") nil 'prepend)
+(set-fontset-font t 'emoji (font-spec :family "Apple Color Emoji"))
+(set-fontset-font t 'ucs (font-spec :family "SF Pro") nil 'prepend)
 
-(setopt global-hl-line-sticky-flag nil
-        global-hl-line-mode t
-        global-display-fill-column-indicator-mode t
-        global-display-line-numbers-mode t
+;;; Highlighting
+;; (setopt global-hl-line-sticky-flag nil
+;;         global-hl-line-mode t)
+
+;;; Columns and line numbers
+
+;; (setopt global-display-fill-column-indicator-mode t
+;;         display-line-numbers-widen t
+;;         display-line-numbers-width 4
+;;         global-display-line-numbers-mode t)
+
+;;; Cursor and matching
+(setopt cursor-type '(bar . 1)
         x-stretch-cursor t
-        cursor-type '(bar . 1)
-        display-line-numbers-widen t
-        display-line-numbers-width 4
-        display-fill-column-indicator-warning t
+        blink-cursor-mode nil)
+
+(setopt default-input-method nil
         show-paren-delay 0.0125
         show-paren-context-when-offscreen t
         show-paren-mode t
-        show-paren-not-in-comments-or-strings 'on-mismatch
-        default-input-method nil
-        blink-cursor-mode nil)
+        show-paren-not-in-comments-or-strings 'on-mismatch)
 
 
 ;;; Org and notes
+(require 'seq)
 (require 'org)
 
+;;; Org files
 (setopt org-directory "/Users/sthenno/Developer/op1/"
-        org-persist-directory (locate-user-emacs-file "org-persist/")
-        org-ellipsis ""
+        org-persist-directory (locate-user-emacs-file "org-persist/"))
+
+;;; Org display
+(setopt org-startup-truncated t
         org-startup-with-link-previews t
-        org-link-elisp-confirm-function nil
-        org-startup-truncated t
-        org-use-property-inheritance t
-        org-babel-uppercase-example-markers t
+        org-ellipsis " (*)"
         org-hide-emphasis-markers t
         org-hide-macro-markers t
         org-hide-drawer-startup t
-        org-special-ctrl-a t
         org-image-align 'left
-        org-image-max-width 0.60
-        org-attach-method 'cp
+        org-image-max-width 0.60)
+
+;;; Org interaction
+(setopt org-link-elisp-confirm-function nil
+        org-special-ctrl-a t
         org-return-follows-link t
         org-support-shift-select t
+        org-use-property-inheritance t)
+
+(setopt org-attach-method 'cp)
+
+;;; Org Babel
+(setopt org-babel-uppercase-example-markers t
         org-confirm-babel-evaluate nil
-        org-src-preserve-indentation t
-        org-src-content-indentation 0
-        org-edit-src-persistent-message nil
-        org-src-fontify-natively t
-        org-src-tab-acts-natively t
-        org-src-ask-before-returning-to-edit-buffer nil
         org-babel-update-intermediate t
         org-babel-load-languages '((emacs-lisp . t)
                                    (python . t)
                                    (shell . t)))
 
+;;; Org source editing
+(setopt org-src-fontify-natively t
+        org-src-preserve-indentation t
+        org-src-content-indentation 0
+        org-edit-src-persistent-message nil
+        org-src-tab-acts-natively t
+        org-src-ask-before-returning-to-edit-buffer nil)
+
+;;; Denote
 (require 'denote)
 (setopt denote-directory org-directory
         denote-file-type 'org
@@ -196,14 +235,17 @@
         denote-save-buffers t
         denote-kill-buffers t
         denote-open-link-function #'find-file
-        denote-org-front-matter "#+TITLE: %1$s\n\n"
-        denote-buffer-name-prefix "[uncodified] "
         denote-sort-dired-default-reverse-sort t
         denote-rename-buffer-mode t)
 
+(setopt denote-buffer-name-prefix "[D] "
+        denote-org-front-matter "#+TITLE: %1$s\n\n")
+
+;;; Denote Org
 (require 'denote-org)
 (setopt denote-org-store-link-to-heading 'context)
 
+;;; Denote journal
 (require 'denote-journal)
 (setopt denote-journal-title-format "%e %B %Y"
         denote-journal-directory (file-name-concat denote-directory "stages")
@@ -239,9 +281,9 @@
   (interactive)
   (sthenno/denote-journal-find-stages-file-date 1))
 
-(with-eval-after-load 'org
-  (keymap-set org-mode-map "s-<up>" #'sthenno/denote-journal-entry-previous)
-  (keymap-set org-mode-map "s-<down>" #'sthenno/denote-journal-entry-next))
+(dolist (binding '(("s-<up>" . sthenno/denote-journal-entry-previous)
+                   ("s-<down>" . sthenno/denote-journal-entry-next)))
+  (keymap-set org-mode-map (car binding) (cdr binding)))
 (keymap-global-set "s-j" #'denote-journal-new-or-existing-entry)
 
 
@@ -255,50 +297,54 @@
   (setopt xref-search-program 'ripgrep))
 
 
-;;; Templates
-(require 'yasnippet)
-(setopt yas-triggers-in-field t)
-(setopt yas-global-mode t)
-
-
 ;;; Completion and minibuffer
+
+;; Completion defaults
 (setopt text-mode-ispell-word-completion 'completion-at-point
-        tab-always-indent 'complete
-        echo-keystrokes 0.125
+        tab-always-indent 'complete)
+
+;; Minibuffer
+(setopt echo-keystrokes 0.125
         resize-mini-windows t
         help-window-select nil
         read-extended-command-predicate #'command-completion-default-include-p
         minibuffer-default-prompt-format " [%s]"
-        minibuffer-visible-completions nil
+        minibuffer-visible-completions nil)
+
+;; Completion styles
+(setopt completion-styles '(flex)
         completion-cycle-threshold nil
         completions-sort 'historical
         completion-eager-display 'auto
         completion-eager-update 'auto
-        completion-styles '(flex)
         completion-ignore-case nil
         read-file-name-completion-ignore-case t
         read-buffer-completion-ignore-case t
         file-name-shadow-mode t)
 
+;; Vertico
 (require 'vertico)
-(require 'vertico-directory)
 (setopt vertico-resize t
         vertico-count-format (cons "[ %-6s ] " "%s of %s"))
-(keymap-set vertico-map "<tab>" #'vertico-insert)
-(keymap-set vertico-map "<return>" #'vertico-directory-enter)
-(keymap-set vertico-map "<backspace>" #'vertico-directory-delete-char)
+
+(dolist (binding '(("<tab>" . vertico-insert)
+                   ("<return>" . vertico-directory-enter)
+                   ("<backspace>" . vertico-directory-delete-char)))
+  (keymap-set vertico-map (car binding) (cdr binding)))
 (setopt vertico-mode t)
 
+;; Marginalia
 (require 'marginalia)
 (setopt marginalia-mode t)
 
+;; Consult
 (require 'consult)
-(require 'consult-imenu)
-(keymap-global-set "s-b" #'consult-buffer)
-(keymap-global-set "C-s" #'consult-line)
-(keymap-global-set "s-m" #'consult-imenu)
-(keymap-global-set "s-n" #'consult-recent-file)
+(dolist (binding '(("s-b" . consult-buffer)
+                   ("C-s" . consult-line)
+                   ("s-m" . consult-imenu-multi)))
+  (keymap-global-set (car binding) (cdr binding)))
 
+;; Dabbrev
 (require 'dabbrev)
 (defun sthenno/dabbrev-elisp ()
   "Tune `dabbrev' for `emacs-lisp-mode'."
@@ -313,34 +359,41 @@
 (dolist (mode '(authinfo-mode doc-view-mode pdf-view-mode tags-table-mode))
   (add-to-list 'dabbrev-ignored-buffer-modes mode))
 
+;; Spelling
 (setopt ispell-program-name "aspell"
         ispell-save-corrections-as-abbrevs t)
 
+;; Corfu
 (require 'corfu)
-(require 'corfu-history)
-(require 'corfu-popupinfo)
+
+(defun sthenno/corfu-confirm ()
+  "Accept the current Corfu candidate, sending it to Eshell when appropriate."
+  (interactive)
+  (if (derived-mode-p 'eshell-mode)
+      (corfu-send)
+    (corfu-insert)))
+
 (defun sthenno/corfu-eshell-setup ()
   "Use a more conservative Corfu setup in Eshell."
   (setopt-local corfu-auto nil)
-  (setopt corfu-mode t)
-  (keymap-set corfu-map "RET" #'corfu-send))
+  (corfu-mode 1))
+
 (setopt corfu-auto t
         corfu-auto-delay 0.05
         corfu-auto-prefix 2
         corfu-preview-current 'insert
         corfu-popupinfo-delay '(0.025 . 0.05))
-(keymap-set corfu-map "<down>" #'corfu-next)
-(keymap-set corfu-map "TAB" #'corfu-complete)
-(keymap-set corfu-map "<up>" #'corfu-previous)
-(keymap-set corfu-map "<escape>" #'corfu-quit)
-(keymap-set corfu-map "RET" #'corfu-insert)
+(dolist (binding '(("<down>" . corfu-next)
+                   ("TAB" . corfu-complete)
+                   ("<up>" . corfu-previous)
+                   ("<escape>" . corfu-quit)
+                   ("RET" . sthenno/corfu-confirm)))
+  (keymap-set corfu-map (car binding) (cdr binding)))
 (add-hook 'eshell-mode-hook #'sthenno/corfu-eshell-setup)
 (add-hook 'prog-mode-hook #'corfu-popupinfo-mode)
-(setopt corfu-history-mode t)
-(setopt global-corfu-mode t)
+(corfu-history-mode 1)
+(global-corfu-mode 1)
 
-(with-eval-after-load 'savehist
-  (add-to-list 'savehist-additional-variables 'corfu-history))
 (with-eval-after-load 'completion-preview
   (setopt completion-preview-sort-function corfu-sort-function))
 
@@ -356,23 +409,23 @@
 
 ;;; AI
 (require 'gptel)
-(require 'gptel-openai)
 
 (keymap-global-set "s-p" #'gptel)
-(keymap-set gptel-mode-map "s-<return>" #'gptel-send)
-(keymap-set org-mode-map "s-<return>" #'gptel-send)
+(dolist (map (list gptel-mode-map org-mode-map))
+  (keymap-set map "s-<return>" #'gptel-send))
+
 (setopt gptel-default-mode 'org-mode
         gptel-org-branching-context t
-        gptel-track-media t
+        gptel-track-media t)
+(setopt gptel-model 'sthenno
         gptel-max-tokens 32768
         gptel-temperature 1.0
         gptel-backend (gptel-make-openai "sthenno"
                         :protocol "http"
-                        :host "netzach.local:8000"
+                        :host "netzach:8000"
                         :endpoint "/v1/chat/completions"
                         :stream t
                         :key "sk-tmp"
-                        :models '(sthenno))
-        gptel-model 'sthenno)
+                        :models '(sthenno)))
 
 (provide 'init)
